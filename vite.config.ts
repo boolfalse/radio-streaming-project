ba6f36a35ac515244c745578d6aa618ac7feb953
boolfalse <extravaganza1024@gmail.com>
2023-10-30 02:34:24 -0700

    setup & asset files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+    cssMinify: process.env.APP_ENV === 'production',
+  },
   plugins: [react()],
-})
+  server: {
+    https: process.env.APP_ENV === 'production',
+    proxy: {
+      '/api': {
+        target: `http://localhost:${process.env.VITE_BACKEND_PORT || 3001}`,
+        changeOrigin: true,
+        secure: true,
+      },
+      '/socket.io': {
+        target: `http://localhost:${process.env.VITE_BACKEND_PORT || 3001}`,
+        ws: true,
+      },
+    }
+  },
+});
